612682bee51cd48e6c26721948dd102b22fd4217
indra <149474572+indrarudiansyah@users.noreply.github.com>
2024-06-13 12:55:07 +0700

    commit

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -80,7 +80,7 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid">
-                        <h1 class="mt-4">Data Penjualan</h1>
+                        <h1 class="mt-4">Data Penjualan </h1>
                         <ol class="breadcrumb mb-4">
                         </ol>
             </div>
